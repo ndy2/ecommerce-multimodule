2b7d210b8ef9a24e89ee6e7a2188e55b8b686096
ndy2 <emrdbs12@gmail.com>
2022-09-28 11:13:12 +0900

    feat: util 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ include("config-server")
 include("order-service")
 include("user-service")
 include("product-service")
+include("util")
 
 rootProject.children
     .forEach { it.buildFileName = it.name + ".gradle.kts" }
---
+++ util/util.gradle.kts
@@ -0,0 +1,19 @@
+import org.gradle.api.tasks.bundling.Jar
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+description = "유틸"
+
+plugins {
+    id("application")
+}
+
+val jar: Jar by tasks
+val bootJar: BootJar by tasks
+
+bootJar.enabled = false
+jar.enabled = true
+
+
+dependencies {
+
+}
