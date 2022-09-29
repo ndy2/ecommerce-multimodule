f501c4431b69dd051dbd559a4b47f7f3d0c3833a
ndy2 <emrdbs12@gmail.com>
2022-09-29 22:57:25 +0900

    build: datasource 설정 변경

--- order-service/order-service.gradle.kts
+++ order-service/order-service.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 }
 
 dependencies {
+    implementation("mysql:mysql-connector-java:8.0.30")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.kafka:spring-kafka:2.9.1")
